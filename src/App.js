2e91d53f3260356759617696fdec06696d4354f1
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-29 15:24:07 -0400

    added checkout componen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header/index'
 import Home from './components/Home/index'
+import Checkout from './components/Checkout/index'
+
+
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Home />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
+      </Routes>
     </div>
+   
   );
 }
 
---
+++ src/components/Checkout/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+const index = () => {
+  return (
+    <div className='checkout'>
+        <div className='checkout-left'>
+            <img className='checkout-ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt='' />
+            <div>
+                <h2 className='checkout-title'>
+                    Your Shopping Cart
+                </h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
