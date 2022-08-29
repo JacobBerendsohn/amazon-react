18b506002d0ff389033e0d5c643c948cd47da457
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-29 14:27:38 -0400

    Made products dynamic using props

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,19 +2,27 @@ import React from 'react'
 import './index.css'
 import Product from '../Product/index'
 
+const testTitle = 'Test Item';
+const testImg = 'https://www.ondemandcmo.com/wp-content/uploads/2016/03/canstockphoto22402523-arcos-creator.com_-1024x1024.jpg'
+const testPrice = 19.99;
+const testRating = 5;
+
 const index = () => {
   return (
     <div className='home'>
         <div className='home-container'>
             <img className='home-image' src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg' alt='' />
             <div className='home-row'>
-                <Product />
+                <Product title={testTitle} image={testImg} price={testPrice} rating={testRating} />
+                <Product title={testTitle} image={testImg} price={testPrice} rating={testRating} />
             </div>
             <div className='home-row'>
-
+                <Product title={testTitle} image={testImg} price={testPrice} rating={testRating} />
+                <Product title={testTitle} image={testImg} price={testPrice} rating={testRating} />
+                <Product title={testTitle} image={testImg} price={testPrice} rating={testRating} />
             </div>
             <div className='home-row'>
-
+                <Product title={testTitle} image={testImg} price={testPrice} rating={testRating} />
             </div>
         </div>
     </div>
--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import './index.css'
 
-const index = () => {
+const index = ({ title, image, price, rating }) => {
   return (
     <div className='product'>
         <div className='product-info'>
-            <p>Test Product</p>
+            <p>{title}</p>
             <p className='product-price'>
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className='product-rating'>
-                <p>⭐</p>
+                {Array(rating).fill().map((_, i) => (
+                    <p>⭐</p>
+                ))}
             </div>
         </div>
-        <img className='product-image' src='https://www.ondemandcmo.com/wp-content/uploads/2016/03/canstockphoto22402523-arcos-creator.com_-1024x1024.jpg' alt='' />
+        <img className='product-image' src={image} alt='' />
         <button className='product-button'>
             Add to Cart
         </button>
