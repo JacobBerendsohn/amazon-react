7f030d19a18710d06a83c811d74243a832415b03
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-08-29 12:56:44 -0400

    Added header with css and logo

---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './index.css'
+
+const index = () => {
+  return (
+    <div className='header'>
+        <img src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' className='header-logo' />
+        <div className='header-search'>
+            <input className='header-searchInput' type='text' />
+        </div>
+        <div className='header-nav'>
+            <div className='header-option'>
+                <span className='header-optionLineOne'>
+                    Hello Guest
+                </span>
+                <span className='header-optionLineTwo'>
+                    Sign In
+                </span>
+            </div>
+            <div className='header-option'>
+                <span className='header-optionLineOne'>
+                    Returns
+                </span>
+                <span className='header-optionLineTwo'>
+                    & Orders
+                </span>
+            </div>
+            <div className='header-option'>
+                <span className='header-optionLineOne'>
+                    Your
+                </span>
+                <span className='header-optionLineTwo'>
+                    Prime
+                </span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default index
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const index = () => {
+  return (
+    <div className='home'>
+        <h2 className='title home'>
+            HOME
+        </h2>
+    </div>
+  )
+}
+
+export default index
